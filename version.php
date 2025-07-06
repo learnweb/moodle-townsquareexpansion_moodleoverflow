ff67d3e5ff5a869f44302d441cc116585b330a66
tamaro <tamarowalter@yahoo.de>
2025-07-06 23:52:31 +0200

    fix supported array

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2025070600;
 $plugin->requires = 2022112819; // Require Moodle 4.1.
-$plugin->supported = [401, 404, 405, 500];
+$plugin->supported = [401, 500];
 $plugin->component = 'townsquareexpansion_moodleoverflow';
 $plugin->maturity = MATURITY_RC;
 $plugin->release = 'v5.0-rc1';
